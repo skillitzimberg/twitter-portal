7bc9257514b50129be54bea2864a3222c92e6dd1
Scott Bergler <commandinghands@gmail.com>
2021-09-09 17:37:13 -0700

    Add PublicMetrics class to handle additional tweet data

--- Models/Tweet.cs
+++ Models/Tweet.cs
@@ -1,3 +1,5 @@
+using System.Text.Json.Serialization;
+
 namespace TwitterApp.Models
 {
     public class Tweet
@@ -8,5 +10,17 @@ namespace TwitterApp.Models
         }
         public string Id { get; set; }
         public string Text { get; set; }
+        public PublicMetrics PublicMetrics { get;  set; }
+    }
+    
+    public class PublicMetrics {
+        [JsonPropertyName("retweet_count")]
+        public int RetweetCount { get; set; }
+        [JsonPropertyName("reply_count")]
+        public int ReplyCount { get; set; }
+        [JsonPropertyName("like_count")]
+        public int LikeCount { get; set; }
+        [JsonPropertyName("quote_count")]
+        public int QuoteCount { get; set; }
     }
 }
